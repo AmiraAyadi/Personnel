91f87a1fff52a3117284eb7baaf2f7202c7e6271
Amira AYADI <ayadi.amira01@gmail.com>
2017-01-26 13:35:10 +0100

    Fonction d'envoi mail
    Envoie d'e-mail avec piece jointe pdf

---
+++ envoi_mail.py
@@ -0,0 +1,60 @@
+import email.message
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+
+
+
+liste_mail=[]
+liste_nom=[]
+
+e=input("Votre addresse mail\n")
+mdp=input("Votre mot de passe\n")
+
+numero=int(input("combien d'expediteur ?\n"))
+for i in range(0,numero):
+    liste_mail.append(input("entrer l'adresse\n"))
+    print("Quel est le nom de la personne ( que l'on va voir apres le bonjour dans le mail!")
+    liste_nom.append(input())
+print("recap : \n")
+for i,j in (zip(liste_nom,liste_mail)):
+	print(i,j)
+
+ob=input("Quel est l'objet ? \n")
+for i in range(numero):
+
+    msg = MIMEMultipart()
+    
+    pdfAttachment = MIMEApplication("C:/Users/Amira AYADI/Desktop/CV-Imène-AYADI.pdf", _subtype = "pdf")
+    pdfAttachment.add_header('content-disposition', 'attachment', filename = ('CV-Imène-AYADI.pdf'))
+    text = MIMEMultipart('alternative')
+    text.attach(MIMEText(u"""Bonjour %s,
+
+            Je me permets de vous faire parvenir mon CV dans le cas où vous seriez à la recherche d’une assistante mise en scene.
+            Je trouve les candidatures spontanées assez intrusives et très peu fructueuses mais c'est essentiellement grâce à ce genre de mail que je suis engagée si je tombe au bon moment :) 
+
+            Je suis une jeune femme extremement efficace, reactive et motivée et je serai ravie de vous rencontrer dans le  cadre d’un entretien.
+
+            Bien à vous, 
+            Imène AYADI
+            0781956633
+
+    """ % str(liste_nom[i]), "plain", _charset="utf-8"))
+    msg = MIMEMultipart('mixed')
+    msg.attach(text)
+    msg.attach(pdfAttachment)
+
+   
+    msg['Subject'] = ob
+    msg['From'] = e
+    msg['To'] = liste_mail[i]
+
+    server = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    server.starttls()
+    server.login(e, mdp)
+    
+    server.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print("envoyé")
